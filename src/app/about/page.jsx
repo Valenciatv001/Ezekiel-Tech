53a7107ab6188689400f252cd47858a12794f6bb
Valenciatv001 <67506940+Valenciatv001@users.noreply.github.com>
2023-09-22 16:00:17 +0100

    update

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -7,17 +7,17 @@ const AboutPage = () => {
     <div>
       <h1>About Ezekiel</h1>
       <p>
-        I'm Ezekiel, the mind behind this webapp dedicated to all things web development and data analysis. With a passion for technology that burns brighter than the Lagos sun, I've spent years honing my skills as a software engineer and data analyst.</p>
+        I&apos;m Ezekiel, the mind behind this webapp dedicated to all things web development and data analysis. With a passion for technology that burns brighter than the Lagos sun, I&apos;ve spent years honing my skills as a software engineer and data analyst.</p>
 
         <p><b>Tech Enthusiast:</b> I breathe JavaScript, TypeScript, and Node.js. From crafting seamless user interfaces with React to diving into the data world with SQL and MongoDB, I've done it all. My toolkit includes everything from Express.js for robust backends to Next.js for server-side rendering.</p>
 
-        <p><b>Full Stack Dev & More:</b> I'm not just about coding; I'm about creating. As a full stack developer, I believe in building end-to-end solutions. But that's not all. I'm a PHP enthusiast, a CSS magician, and a fan of Tailwind CSS for crafting sleek and responsive designs.</p>
+        <p><b>Full Stack Dev & More:</b> I&apos;m not just about coding; I&apos;m about creating. As a full stack developer, I believe in building end-to-end solutions. But that&apos;s not all. I&apos;m a PHP enthusiast, a CSS magician, and a fan of Tailwind CSS for crafting sleek and responsive designs.</p>
 
         <p><b>Data Whisperer:</b> Data analysis is my secret superpower. I thrive on deciphering data, uncovering insights, and turning numbers into stories. SQL is my trusted companion for database wizardry, and I dance with data using tools like Microsoft Excel and Python.</p>
 
-        <p><b>Education & Sharing:</b> Knowledge is power, and I'm a firm believer in sharing it. I've been known to dive deep into tech books and admire the wonders of space engineering. My ultimate goal? To help you become a tech wizard, whether you're just starting out or on a quest for expertise.</p>
+        <p><b>Education & Sharing:</b> Knowledge is power, and I&apos;m a firm believer in sharing it. I&apos;ve been known to dive deep into tech books and admire the wonders of space engineering. My ultimate goal? To help you become a tech wizard, whether you&apos;re just starting out or on a quest for expertise.</p>
 
-        <p>Join me on this journey of learning, problem-solving, and innovation. Together, we'll master the ever-evolving world of web development and data analysis. Let's build a brighter, tech-savvier future, one code at a time.</p>
+        <p>Join me on this journey of learning, problem-solving, and innovation. Together, we&apos;ll master the ever-evolving world of web development and data analysis. Let&apos;s build a brighter, tech-savvier future, one code at a time.</p>
  
     </div>
   );
